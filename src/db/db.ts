a2bf473daa8cb9923259df4d23af19994887fd85
Varun Singh <varunsingh7004@gmail.com>
2025-08-14 08:38:54 +0530

    db made good

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
-mongoose
-  .connect(
-    "mongodb+srv://varunsingh7004:vader@cluster0.bznjjdn.mongodb.net/varunsingh"
-  )
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+const connectDB = async (): Promise<void> => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is not defined in environment variables");
+    }
+    const db = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`\n MongoDB connected !! DB HOST:${db.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
